01b1a828f972c744af4f4f85ad67c81bc191184e
Miguelkoro <miguelkoro@gmail.com>
2025-06-05 20:32:29 +0200

    mergeada la caja fuerte, quedaria borrar cosas innecesarias, pasar las variables al constants

--- src/components/MainScreen.jsx
+++ src/components/MainScreen.jsx
@@ -1,24 +1,65 @@
 import React, { useState, useEffect, useContext } from 'react';
 import { GlobalContext } from "./GlobalContext";
 import './../assets/scss/main.scss';
-
+import SafeBoxDial from './SafeBoxDial.jsx';
 import BoxButton from './BoxButton.jsx';
 
 const MainScreen = (props) => {
   const { escapp, appSettings, Utils, I18n } = useContext(GlobalContext);
+  const [tries, setTries] = useState(0); // Contador de intentos
+  const [solutionArray, setSolutionArray] = useState([]); // Array para guardar la solución
   const [currentSolution, setCurrentSolution] = useState([]);
   const [processingSolution, setProcessingSolution] = useState(false);
   const [light, setLight] = useState("off");
-  const [containerWidth, setContainerWidth] = useState(0);
-  const [containerHeight, setContainerHeight] = useState(0);
-  const [containerMarginTop, setContainerMarginTop] = useState(0);
-  const [containerMarginLeft, setContainerMarginLeft] = useState(0);
+  const [containerWidth, setContainerWidth] = useState(0);//
+  const [containerHeight, setContainerHeight] = useState(0);//
+  const [containerMarginTop, setContainerMarginTop] = useState(0);//
+  const [containerMarginLeft, setContainerMarginLeft] = useState(0);//
   const [boxWidth, setBoxWidth] = useState(0);
   const [boxHeight, setBoxHeight] = useState(0);
-  const [lightWidth, setLightWidth] = useState(0);
-  const [lightHeight, setLightHeight] = useState(0);
-  const [lightLeft, setLightLeft] = useState(0);
-  const [lightTop, setLightTop] = useState(0);
+  const [lightWidth, setLightWidth] = useState(0); //
+  const [lightHeight, setLightHeight] = useState(0); //
+  const [lightLeft, setLightLeft] = useState(0);//
+  const [lightTop, setLightTop] = useState(0);//
+
+  //
+  const [rotationAngle, setRotationAngle] = useState(0); // Estado para la rotación
+  const [isReseting, setIsReseting] = useState(false); // Estado para saber si se está reiniciando el lock
+
+  //
+
+  const styles ={
+    "STANDARD": {
+      lock: "../images/standard/lock_classic.png",
+      dial: "images/standard/dial_classic.png",
+      dial_size: 0.4,
+      dial_sound: "sounds/spin.wav",
+      dial_text_color: "#000000",
+      dial_text_size: "13vmin",
+      dial_text_zIndex: 1,
+    },
+    "RETRO": {
+      lock: "images/lock_old.png",
+      dial: "images/dial_old.png",
+      dial_size: 0.5,
+      dial_sound: "sounds/spin_old2.wav",
+      dial_text_color:  "#FFFFFF",
+      dial_text_size: "10vmin",
+      dial_text_zIndex: 1,
+    },
+    "FUTURISTIC": {
+      lock: "images/lock_modern.png",
+      dial: "images/dial_modern.png",
+      dial_size: 0.6,
+      dial_sound: "sounds/spin.wav",
+      dial_text_color: "#59c2ca",
+      dial_text_size: "11vmin",
+      dial_text_zIndex: -1,
+    }
+  }
+  const defaultStyle = styles["STANDARD"];
+  const style = styles[appSettings.skin] || styles["STANDARD"];
+//
 
   useEffect(() => {
     handleResize();
@@ -33,18 +74,25 @@ const MainScreen = (props) => {
     let _keypadWidth = Math.min(props.appHeight * aspectRatio, props.appWidth);
     let _keypadHeight = _keypadWidth / aspectRatio;
 
-    let _containerWidth = _keypadWidth * 0.22;
-    let _containerHeight = _keypadHeight * 0.4;
+    let _lockWidth = Math.min(props.appHeight * aspectRatio, props.appWidth) ;
+    let _lockHeight = _lockWidth / aspectRatio;
+
+    let _containerWidth = _lockWidth *0.8;
+    let _containerHeight = _lockHeight *0.8;
+
+
     let _containerMarginLeft;
     let _containerMarginTop;
 
-    let _boxWidth = _keypadWidth * 0.06;
-    let _boxHeight = _keypadHeight * 0.1;
+    let _boxWidth = _lockWidth * 0.7;
+    let _boxHeight = _lockHeight * 0.7;
 
     let _lightWidth;
     let _lightHeight;
     let _lightLeft;
     let _lightTop;
+
+
 
     switch(appSettings.skin){
       case "RETRO":
@@ -65,12 +113,12 @@ const MainScreen = (props) => {
         break;
       default:
         //Standard skin
-        _containerMarginTop = 0;
-        _containerMarginLeft = _keypadWidth * 0.045;
-        _lightWidth = _keypadWidth * 0.05;
-        _lightHeight = _keypadHeight * 0.5;
-        _lightLeft = props.appWidth / 2 + _keypadWidth / 2 * 0.35;
-        _lightTop = props.appHeight / 2 - _keypadHeight / 2 * 0.8;
+        //_containerMarginTop = 0;
+        //_containerMarginLeft = _keypadWidth * 0.045;
+        _lightWidth = _lockWidth * 0.08;
+        _lightHeight = _lockHeight * 0.08;
+        _lightLeft = props.appWidth / 2 + _lockWidth / 2 * 0.15;
+        _lightTop = props.appHeight / 2 - _lockHeight / 2 * 0.82
     }
 
     setContainerWidth(_containerWidth);
@@ -122,6 +170,25 @@ const MainScreen = (props) => {
     }, 300);
   }
 
+  const checkSolution = () => {
+    setProcessingSolution(true);
+    Utils.log("Check solution", solutionArray);
+    const solution = solutionArray.join(';');
+    //const solution="12315"
+    reset(); // Reinicia el lock
+    console.log("Check solution", solution);
+    escapp.checkNextPuzzle(solution, {}, (success, erState) => {
+          Utils.log("Check solution Escapp response", success, erState);
+          try {
+            setTimeout(() => {
+              changeBoxLight(success, solution);
+            }, 700);
+          } catch(e){
+            Utils.log("Error in checkNextPuzzle",e);
+          }
+        });
+  }
+
   const changeBoxLight = (success, solution) => {
     let audio;
     let afterChangeBoxLightDelay = 1000;
@@ -133,29 +200,53 @@ const MainScreen = (props) => {
     } else {
       audio = document.getElementById("audio_failure");
       setLight("nok");
+      reset(); //
     }
 
     setTimeout(() => {
-      if((success===false)||(appSettings.skin != "RETRO")){
+      if(!success){
         setLight("off");
         setProcessingSolution(false);
-      }
-      if (success) {
-        props.onKeypadSolved(solution);
+      }else{
+        //props.onKeypadSolved(solution); //Cambiar
       }
     }, afterChangeBoxLightDelay);
 
     audio.play();
   }
 
-  let backgroundImage = 'url("' + appSettings.backgroundKeypad + '")';
+  //Pone la imagen del fondo
+  //let backgroundImage = 'url("' + appSettings.backgroundKeypad + '")';
+  let backgroundImage = 'url("' + appSettings.background + '")';
   if(appSettings.background && appSettings.background !== "NONE"){
     backgroundImage += ', url("' + appSettings.background + '")';
   }
 
+  const  reset = () =>{
+    //console.log("Solution: ", solutionArray);
+    setIsReseting(true);
+    setRotationAngle(0); // Reinicia el ángulo de rotación
+    setSolutionArray([]);
+    //setTries(0);
+    setTimeout(() => {      
+      setIsReseting(false);
+    }, 2500);
+    //setChecking(false);
+  }
+
+  useEffect(() => { // Comprueba si se ha alcanzado el número máximo de intentos (En local y en API)           
+    //console.log("Tries: ", tries, "Solution: ", solutionArray);
+      solutionArray.length >= appSettings.solutionLength && checkSolution();
+      console.log("Solution: ", solutionArray);
+  }, [solutionArray]);
+
   return (
     <div id="screen_main" className={"screen_content"} style={{ backgroundImage: backgroundImage }}>
-      <div id="keypad" style={{ width: containerWidth, height: containerHeight, marginTop: containerMarginTop, marginLeft: containerMarginLeft }}>
+      <div id="lockContainer" className="lockContainer" 
+        style={{backgroundImage: 'url('+appSettings.backgroundLock+')', width: containerWidth, height: containerHeight, marginTop: containerMarginTop, marginLeft: containerMarginLeft ,
+          display: "flex", alignItems: "center", justifyContent: "center",
+        }}>
+      {/*<div id="keypad" style={{ width: containerWidth, height: containerHeight, marginTop: containerMarginTop, marginLeft: containerMarginLeft }}>
         <audio id="audio_beep" src={appSettings.soundBeep} autostart="false" preload="auto" />
         <audio id="audio_failure" src={appSettings.soundNok} autostart="false" preload="auto" />
         <audio id="audio_success" src={appSettings.soundOk} autostart="false" preload="auto" />
@@ -182,6 +273,18 @@ const MainScreen = (props) => {
         <div className="boxLight boxLight_off" style={{ visibility: light === "off" ? "visible" : "hidden", opacity: light === "off" ? "1" : "0", width: lightWidth, height: lightHeight, backgroundImage: 'url("' + appSettings.imageLightOff + '")', left: lightLeft, top: lightTop }} ></div> 
         <div className="boxLight boxLight_nok" style={{ visibility: light === "nok" ? "visible" : "hidden", opacity: light === "nok" ? "1" : "0", width: lightWidth, height: lightHeight, backgroundImage: 'url("' + appSettings.imageLightNok + '")', left: lightLeft, top: lightTop }} ></div> 
         <div className="boxLight boxLight_ok" style={{ visibility: light === "ok" ? "visible" : "hidden", opacity: light === "ok" ? "1" : "0", width: lightWidth, height: lightHeight, backgroundImage: 'url("' + appSettings.imageLightOk + '")', left: lightLeft, top: lightTop }} ></div> 
+      </div>*/}
+      <SafeBoxDial styles={style}
+              boxWidth={boxWidth} boxHeight={boxHeight} checking={processingSolution} 
+              rotationAngle={rotationAngle} setRotationAngle={setRotationAngle}
+              setSolutionArray={setSolutionArray} isReseting={isReseting}/>
+        <div className="boxLight boxLight_off" style={{ visibility: light === "off" ? "visible" : "hidden", opacity: light === "off" ? "1" : "0", width: lightWidth, height: lightHeight, backgroundImage: 'url("' + appSettings.imageLightOff + '")', left: lightLeft, top: lightTop }} ></div> 
+        <div className="boxLight boxLight_nok" style={{ visibility: light === "nok" ? "visible" : "hidden", opacity: light === "nok" ? "1" : "0", width: lightWidth, height: lightHeight, backgroundImage: 'url("' + appSettings.imageLightNok + '")', left: lightLeft, top: lightTop }} ></div> 
+        <div className="boxLight boxLight_ok" style={{ visibility: light === "ok" ? "visible" : "hidden", opacity: light === "ok" ? "1" : "0", width: lightWidth, height: lightHeight, backgroundImage: 'url("' + appSettings.imageLightOk + '")', left: lightLeft, top: lightTop }} ></div> 
+
+      <audio id="audio_beep" src={appSettings.soundBeep} autostart="false" preload="auto" />
+      <audio id="audio_failure" src={appSettings.soundNok} autostart="false" preload="auto" />
+      <audio id="audio_success" src={appSettings.soundOk} autostart="false" preload="auto" />
       </div>
     </div>);
 };
---
+++ src/components/SafeBoxDial.jsx
@@ -0,0 +1,118 @@
+import './../assets/scss/main.scss';
+import { useState, useEffect, useContext } from 'react';
+import { GlobalContext } from "./GlobalContext";
+
+const  SafeBoxDial = ( props ) => {
+    const {  appSettings } = useContext(GlobalContext);
+    const [initialRotation, setInitialRotation] = useState(0); // Ángulo inicial del lock
+    const [isMouseDown, setIsMouseDown] = useState(false); // Estado para saber si el mouse está presionado
+    const [startAngle, setStartAngle] = useState(0); // Ángulo inicial del ratón
+    const [rotationDirection, setRotationDirection] = useState(""); // Dirección de rotación
+
+
+    const handleMouseMove = (event) => {
+        if (!isMouseDown || props.checking || props.isReseting) return ; // Solo ejecuta si el mouse está presionado    
+        let audio  = document.getElementById("audio_wheel");
+        let rounded = calculateAngle(event); // Calcula el ángulo 
+       // Calcula la diferencia de ángulos de forma cíclica
+        const angleDifference = normalizeAngleDifference(rounded - startAngle);
+       // Calcula la rotación acumulada y normalízala
+        const newRotation = normalizeAngle(initialRotation + angleDifference);
+        const rotationDir = getRotationDirection(props.rotationAngle/6, newRotation/6);
+        //Si se intenta girar en sentido contrario a la rotacion actual, no se hace nada
+        if(rotationDirection === ''){
+          setRotationDirection(rotationDir);
+        }else if(rotationDirection !== rotationDir){
+          return;}
+        if(props.rotationAngle === newRotation)return; // No actualiza si el ángulo no ha cambiado
+        props.setRotationAngle(newRotation);     // Actualiza el ángulo de rotación
+        audio.play();
+    };
+
+    const handleMouseUp = () => {
+        if (props.checking || props.isReseting || props.rotationAngle==[]) return ;
+        setIsMouseDown(false); // Indica que el mouse ya no está presionado
+        //reset(); // Reinicia la rotación //Poniendolo aqui, hace efecto de teelfono de dial
+        //Para poder poner -55 si va contrarreloj o 30 si va a favor
+        props.setSolutionArray((sol) => [...sol, (rotationDirection === "clockwise" ? props.rotationAngle/6 : -props.rotationAngle/6)]);
+        setRotationDirection(''); //Reinicia la direccion de rotacion
+    };
+
+    const handleMouseDown = (event) => {
+        if (props.checking || props.isReseting) return ;
+        setIsMouseDown(true); // Indica que el mouse está presionado    
+        let rounded = calculateAngle(event); // Calcula el ángulo inicial
+        setStartAngle(rounded);     // Guarda el ángulo inicial y el ángulo actual del lock
+        setInitialRotation(props.rotationAngle); // Guarda el ángulo actual del lock    
+      };
+
+    const calculateAngle = (event) => {
+        const lockElement = document.getElementById("lock");
+        const rect = lockElement.getBoundingClientRect();  
+        // Calcula el centro del div
+        const centerX = rect.left + rect.width / 2;
+        const centerY = rect.top + rect.height / 2;  
+        // Calcula el ángulo inicial en radianes y lo convierte a grados
+        const radians = Math.atan2(event.clientY - centerY, event.clientX - centerX);
+        let angle = radians * (180 / Math.PI);  
+        // Normaliza el ángulo para que esté entre 0° y 360°
+        if (angle < 0) {
+          angle += 360;}
+        return Math.round(angle / 6) * 6;
+      }
+
+    function getRotationDirection(prev, curr) {
+        const diff = (curr - prev + 60) % 60;
+        if (diff === 0) return '';
+        return diff < 30 ? 'clockwise' : 'counter-clockwise';
+    }
+
+    const normalizeAngleDifference = (angle) => {
+        return ((angle + 180) % 360) - 180;
+    };    
+    const normalizeAngle = (angle) => {
+        return ((angle % 360) + 360) % 360; // Asegura que el ángulo esté entre 0 y 360
+    };
+
+    const reset = () => {
+        setStartAngle(0);
+        setRotationDirection("");
+    }
+
+    useEffect(() => {    
+        if (props.isReseting) { 
+            reset(); // Reinicia el lock
+        }}, [props.isReseting]); // Se ejecuta cuando isReseting cambia
+
+    return(
+        <div className='lockContainer' style={{  
+           // backgroundImage: 'url("' + appSettings.backgroundLock + '")',
+            width: props.boxWidth,//Math.min(props.boxWidth, props.boxHeight) * 0.7, 
+            height: props.boxHeight, //Math.min(props.boxWidth, props.boxHeight) * 0.7,
+            display: "flex", alignItems: "center", justifyContent: "center",}}
+        
+            onDragStart={(event) => event.preventDefault()} 
+            onMouseUp={handleMouseUp} 
+            onMouseDown={handleMouseDown} 
+            onMouseMove={handleMouseMove}>
+          
+            <div id="lock" style={{ 
+              backgroundImage: 'url("' + appSettings.backgroundDial + '")',
+              height:props.boxHeight*0.53,
+              width: props.boxWidth*0.53,
+              zIndex: 2,//props.styles.dial_text_zIndex,
+              //width: Math.min(props.boxWidth, props.boxHeight) * props.styles.dial_size, // Usa el menor valor para asegurar que sea cuadrado
+              //height: Math.min(props.boxWidth, props.boxHeight) * props.styles.dial_size, // Usa el menor valor para asegurar que sea cuadrado
+              //marginLeft: props.boxWidth / 2 * 0.225,
+              transform: `rotate(${props.rotationAngle}deg)`, // Rotación dinámica.
+              transition: props.isReseting ? "transform 2.5s ease" : "none", // Transición suave solo durante el reset
+            }}></div>
+            <p id="rotationNum" className='rotationNum' onDragStart={(event) => event.preventDefault()} 
+              style={{color: props.styles.dial_text_color, fontSize: props.styles.dial_text_size, zIndex:5}}
+              >{props.rotationAngle/6}</p>      
+              <audio id="audio_wheel" src={appSettings.soundDial} autostart="false" preload="auto" />    
+        </div>
+    );
+}
+
+export default SafeBoxDial;
